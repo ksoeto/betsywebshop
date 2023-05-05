867149e748de7215536012dbf1d74ab3d107c832
ksoeto <76516873+ksoeto@users.noreply.github.com>
2023-05-05 13:23:01 +0200

    models.py

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
-# Models go here
-
 import peewee
+import datetime
 from peewee import *
 from playhouse.sqlite_ext import SqliteExtDatabase
 
@@ -10,30 +9,51 @@ db = SqliteExtDatabase('betsywebshop.db', pragmas={
     'cache_size': -64 * 1000, 
     'synchronous': 0})
 
+
 class BaseModel(Model):
     class Meta:
         database = db
 
-class User(Model):
-    username = CharField()
-    join_date = DateTimeField()
-    about_me = TextField()
-    
-class product(BaseModel):
-    name = charField()
-    information= TextField()
-    purchase = DatetimeField()
-    quantity = IntegerField()
-    price = DecimalField()
-    
-class tag(BaseModel):
-    tag = ForeignField(product)
-    
-class transaction(BaseModel):
-    user = ForeignKeyField(User, backref='tweets')
-    message = TextField()
-    purchased_date = DateTimeField(default=datetime.datetime.now)
-    quantity = Integerfield()
+
+class User(BaseModel):
+    name = CharField(unique=True)
+    first_name = CharField()
+    last_name = CharField()
+    address = TextField()
+    postal_code = CharField()
+    city = CharField()
+    country = CharField()  
+    billing_info = CharField()  
+
+
+class Product(BaseModel):
+    name = CharField(unique=True)
+    description = CharField()
+    unit_price = DecimalField(decimal_places=2, max_digits=10)
+    quantity = IntegerField()  
+
+
+class Ownership(BaseModel):
+    user = ForeignKeyField(User)
+    product = ForeignKeyField(Product)
+    number_owned = IntegerField()  
+
+
+class Tag(BaseModel):
+    tag_text = CharField(unique=True)
+
+
+class ProductTag(BaseModel):
+    product = ForeignKeyField(Product)
+    tag = ForeignKeyField(Tag)
+
+
+class Transaction(BaseModel):
+    buyer = ForeignKeyField(User)
+    product = ForeignKeyField(Product)
+    number_sold = IntegerField()
+    datetime_sold = DateTimeField(default=datetime.datetime.now)
+
 
     
     
